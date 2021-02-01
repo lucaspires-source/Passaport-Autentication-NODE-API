60f5bade27ef4ac5faefe6c7abf433558faaeb6e
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-01 16:19:27 -0300

    started workin on routers

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-
+app.use('/',require('./routes/index'))
+app.use('/users',require('./routes/users'))
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
